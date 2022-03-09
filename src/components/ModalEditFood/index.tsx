e110ff529bb7c954ee9aeea7bb636170b8d4ae15
Pedro Henrique de Marins da Silva - PHM <pedro.marins@iteris.com.br>
2022-03-09 11:37:57 -0300

    feat: transforming class components in functional components

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,46 @@
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { useRef } from 'react';
+import { IFood } from '../../types/food/food';
+
+interface ModalEditFoodProps {
+  setIsOpen: () => void;
+  handleUpdateFood: (data: IFood) => void;
+  isOpen: boolean;
+  editingFood: Object;
+}
+
+function ModalEditFood({ setIsOpen, handleUpdateFood, isOpen, editingFood }: ModalEditFoodProps) {
+  const formRef = useRef(null);
+
+  const handleSubmit = (data: IFood) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
+
+export default ModalEditFood;
